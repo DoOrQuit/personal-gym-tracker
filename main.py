8743880874d600fc22fcb228e5566865e640cf9f
Antony_K <kosankos@gmail.com>
2022-12-16 03:06:46 +0200

    added users_list_exists() to check if the users_list.csv exists prior to creating object of Security() class which used to cause an error in case if file was missed

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from frames.security import Security
-from frames.ui import MainInterface
+from core.security import Security
+from core.ui import MainInterface
+from core.system_scripts import users_list_exists
 
 
 def main():
-    login = Security()
+    users_list_exists() # performing a check of existing the users list file
+    authorization = Security()
+    authorization.run()
+    if authorization.security_passed:
+        print("Authorization SUCCESS")
+        app = MainInterface(authorization.user_name.get())
+
+
+
+
+
 
 
 if __name__ == '__main__':
-   main()
-
-
-
+    main()
